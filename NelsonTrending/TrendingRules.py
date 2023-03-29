8271b2831ab49c3f5a604345584626f88c920410
Kirsten <kirsten@regenplatz.de>
2023-03-29 20:57:15 +0200

    code re-designed for dynamic input of rule parameters (e.g. for the number of points to fulfill a condition)

--- NelsonTrending/TrendingRules.py
+++ NelsonTrending/TrendingRules.py
@@ -3,7 +3,6 @@
 __author__ = "WhyKiki"
 __version__ = "1.0.0"
 
-
 import numpy as np
 
 
@@ -18,8 +17,8 @@ class TrendingRules:
         self.df = df
         self.colName = colName
         self.data = self.df[self.colName]
-        self.mean = np.mean(df[colName])
-        self.sigma = np.std(df[colName])
+        self.mean = np.mean(df[colName], axis=0)
+        self.sigma = np.std(df[colName], axis=0)
         ## initialize color code as green and overwrite outlier's color code later
         self.df["colorCode"] = "Green"
 
@@ -33,188 +32,287 @@ class TrendingRules:
         """
         dict_lastElements = {}
         for ind, el in lastElements:
-            dict_lastElements[idx - (len(lastElements)-1) + ind] = el
+            dict_lastElements[idx - (len(lastElements) - 1) + ind] = el
         self.df["colorCode"].iloc[[k for k, v in dict_lastElements.items() if v == min(lastElements)]] = "Yellow"
 
 
-    def rule01(self):
+    def rule05_rule06(self, noPoints_window, noPointsOutOf_window, std_value):
         """
-        One point is more than 3 standard deviations from the mean.
-        :return: dataframe containing outlier color coding.
+        Check if a certain number of points (e.g. 2) from a given window (e.g. 3) are more than x standard deviations
+        (e.g. 2 std) away from the mean and the points are in the same direction (all above or all below mean).
+        :param noPoints_window: int, number of points for window of interest, e.g. 3 in "2 out of 3 points in a row"
+        :param noPointsOutOf_window: int, number of points within this window that fulfill a certain condition,
+                e.g. 2 in "2 out of 3 points in a row"
+        :param std_value: int, number of standard deviations, e.g. 2 for 2 std
+        :return: dataframe containing outlier/trend color coding
         """
-        self.df["colorCode"] = self.df.apply(lambda x: ("Red" if x[self.colName] < (self.mean - 3 * self.sigma) or
-                                             x[self.colName] > (self.mean + 3 * self.sigma) else "Green"), axis=1)
+        for idx, elem in enumerate(self.data):
+            last_noPoints = list(self.data.iloc[idx - (noPoints_window - 1):idx + 1])
+            if len(last_noPoints) == noPoints_window:
+                helperX = sorted(last_noPoints)
+                minX = helperX[len(helperX) - noPointsOutOf_window:]
+
+                ## if elements are greater than the mean
+                if min(last_noPoints) > self.mean:
+                    ## color points in red if condition fulfilled
+                    if min(minX) > self.mean + (std_value * self.sigma):
+                        self.df["colorCode"].iloc[idx - (noPoints_window - 1):idx + 1] = "Red"
+                        ## in case only m out of n points are greater x*sigma, color the remaining ones in yellow
+                        if min(last_noPoints) < (std_value * self.sigma):
+                            self.colorYellow(last_noPoints, idx)
+
+                ## if elements are smaller than the mean
+                elif min(last_noPoints) < self.mean:
+                    ## color points in red if condition fulfilled
+                    if min(minX) > self.mean + (std_value * self.sigma):
+                        self.df["colorCode"].iloc[idx - (noPoints_window - 1):idx + 1] = "Red"
+                        ## in case only m out of n points are greater x*sigma, color the remaining ones in yellow
+                        if min(last_noPoints) > (std_value * self.sigma):
+                            self.colorYellow(last_noPoints, idx)
+
+                else:
+                    pass
 
         return self.df
 
 
-    def rule02(self):
+    def rule01(self, std_value=3):
         """
-        Nine (or more) points in a row are on the same side of the mean.
+        One point is more than x standard deviations from the mean.
+        :param std_value: int, number of standard deviations, e.g. 3 for 3 std
+        :return: dataframe containing outlier color coding.
+        """
+        self.df["colorCode"] = self.df.apply(
+            lambda x: ("Red" if (x[self.colName] < (self.mean - (std_value * self.sigma))) or
+                                (x[self.colName] > (self.mean + (std_value * self.sigma))) else "Green"),
+            axis=1)
+
+        return self.df
+
+
+    def rule02(self, no_points=9):
+        """
+        Number of points (or more) in a row are on the same side of the mean.
+        :param no_points: int, minimum number of points to fulfill the condition
         :return: dataframe containing outlier color coding.
         """
         for idx, elem in enumerate(self.data):
-            # last9 = list(self.data[:idx+1])[-9:]
-            last9 = list(self.data.iloc[idx-8:idx+1])
-            if len(last9) == 9:
-                if min(last9) > self.mean or max(last9) < self.mean:
-                    self.df["colorCode"].iloc[idx-8:idx+1] = "Red"
+            last_noPoints = list(self.data.iloc[idx - (no_points - 1):idx + 1])
+            if len(last_noPoints) == no_points:
+                if min(last_noPoints) > self.mean or max(last_noPoints) < self.mean:
+                    self.df["colorCode"].iloc[idx - (no_points - 1):idx + 1] = "Red"
 
         return self.df
 
 
-    def rule03(self):
+    def rule03(self, no_points=6):
         """
-        Six (or more) points in a row are continually increasing (or decreasing).
+        Number of points (or more) in a row are continually increasing (or decreasing).
+        :param no_points: int, minimum number of points to fulfill the condition
         :return: dataframe containing outlier color coding.
         """
         for idx, elem in enumerate(self.data):
-            last6 = list(self.data.iloc[idx-5:idx+1])
-            if len(last6) == 6:
-                ## continually increasing or decreasing
-                if last6[5] > last6[4] > last6[3] > last6[2] > last6[1] > last6[0] \
-                        or last6[5] < last6[4] < last6[3] < last6[2] < last6[1] < last6[0]:
-                    self.df["colorCode"].iloc[idx-5:idx+1] = "Red"
+
+            last_direction = "start"
+            last_elem = -100
+            cnt = 0
+            last_noPoints = list(self.data.iloc[idx - (no_points - 1):idx + 1])
+            if len(last_noPoints) == no_points:
+
+                for elem2 in last_noPoints:
+                    if last_direction == "start":
+                        direction = None
+                    else:
+                        if elem2 > last_elem:
+                            direction = "up"
+                        elif elem2 < last_elem:
+                            direction = "down"
+                        else:
+                            direction = "equal"
+
+                        if ((last_direction == direction) or (last_direction is None)) and (direction != "equal"):
+                            cnt += 1
+                        else:
+                            break
+
+                    last_direction = direction
+                    last_elem = elem2
+
+            if cnt == (no_points-1):
+                self.df["colorCode"].iloc[idx-(no_points-1):idx+1] = "Red"
 
         return self.df
 
 
-    def rule04(self):
+    def rule04(self, no_points=14):
         """
         Fourteen (or more) points in a row alternate in direction, increasing then decreasing.
         :return: dataframe containing outlier color coding.
         """
         for idx, elem in enumerate(self.data):
-            last14 = list(self.data.iloc[idx-13:idx+1])
-            if len(last14) == 14:
-                if (last14[13] > last14[12] < last14[11] > last14[10] < last14[9] > last14[8] < last14[7] > last14[6] < last14[5] > last14[4] < last14[3] > last14[2] < last14[1] > last14[0]) \
-                        or (last14[13] < last14[12] > last14[11] < last14[10] > last14[9] < last14[8] > last14[7] < last14[6] > last14[5] < last14[4] > last14[3] < last14[2] > last14[1] < last14[0]):
-                    self.df["colorCode"].iloc[idx-14:idx+1] = "Red"
+
+            last_direction = "start"
+            last_elem = -100
+            cnt = 0
+            last_noPoints = list(self.data.iloc[idx-(no_points-1):idx+1])
+            if len(last_noPoints) == no_points:
+
+                for elem2 in last_noPoints:
+                    if last_direction == "start":
+                        direction = None
+                    else:
+                        if elem2 > last_elem:
+                            direction = "up"
+                        elif elem2 < last_elem:
+                            direction = "down"
+                        else:
+                            direction = "equal"
+
+                        if (last_direction != direction) and (direction != "equal"):
+                            cnt += 1
+                        else:
+                            break
+
+                    last_direction = direction
+                    last_elem = elem2
+
+                if cnt == (no_points-1):
+                    self.df["colorCode"].iloc[idx-(no_points-1):idx+1] = "Red"
 
         return self.df
 
 
-    def rule05(self):
+    def rule05(self, noPoints_window=3, noPointsOutOf_window=2, std_value=2):
         """
         Check if 2 out of 3 elements in a row are more than 2 std away from the mean in the same direction.
+        :param noPoints_window: int, number of points for window of interest, e.g. 3 in "2 out of 3 points in a row"
+        :param noPointsOutOf_window: int, number of points within this window that fulfill a certain condition,
+                e.g. 2 in "2 out of 3 points in a row"
+        :param std_value: int, number of standard deviations, e.g. 2 for 2 std
         :return: dataframe containing outlier color coding.
         """
-        for idx, elem in enumerate(self.data):
-            last3 = list(self.data.iloc[idx-2:idx+1])
-            if len(last3) == 3:
-                min2 = last3.copy()
-                min2.remove(min(last3))
-
-                ## if elements are greater than the mean
-                if min(last3) > self.mean:
-                    ## color points in red if condition fulfilled
-                    if min(min2) > self.mean + 2*self.sigma:
-                        self.df["colorCode"].iloc[idx-2:idx+1] = "Red"
-                        ## in case only 2 out of 3 points are greater 2*sigma, color the remaining one in yellow
-                        if min(last3) < 2*self.sigma:
-                            self.colorYellow(last3, idx)
-
-                ## if elements are smaller than the mean
-                elif min(last3) < self.mean:
-                    ## color points in red if condition fulfilled
-                    if min(min2) < self.mean - 2 * self.sigma:
-                        self.df["colorCode"].iloc[idx-2:idx+1] = "Red"
-                        ## in case only 2 out of 3 points are greater 2*sigma, color the remaining one in yellow
-                        if min(last3) > 2 * self.sigma:
-                            self.colorYellow(last3, idx)
+        self.df = self.rule05_rule06(noPoints_window, noPointsOutOf_window, std_value)
 
         return self.df
 
 
-    def rule06(self):
+    def rule06(self, noPoints_window=5, noPointsOutOf_window=4, std_value=1):
         """
         check if 4 out of 5 elements in a row are more than 1 std away from the mean in the same direction.
+        :param noPoints_window: int, number of points for window of interest, e.g. 3 in "2 out of 3 points in a row"
+        :param noPointsOutOf_window: int, number of points within this window that fulfill a certain condition,
+                e.g. 2 in "2 out of 3 points in a row"
+        :param std_value: int, number of standard deviations, e.g. 2 for 2 std
         :return: dataframe containing outlier color coding.
         """
-        for idx, elem in enumerate(self.data):
-            last5 = list(self.data.iloc[idx-4:idx+1])
-            if len(last5) == 5:
-                min4 = last5.copy()
-                min4.remove(min(last5))
-
-                ## if elements are greater than the mean
-                if min(last5) > self.mean:
-                    if min(min4) > self.mean + self.sigma:
-                        self.df["colorCode"].iloc[idx-4:idx+1] = "Red"
-                        ## in case only 2 out of 3 points are greater 2*sigma, color the remaining one in yellow
-                        if min(last5) < 2 * self.sigma:
-                            self.colorYellow(last5, idx)
-
-                ## if elements are smaller than the mean
-                if min(last5) < self.mean:
-                    if min(min4) < self.mean - self.sigma:
-                        self.df["colorCode"].iloc[idx-4:idx+1] = "Red"
-                        ## in case only 2 out of 3 points are greater 2*sigma, color the remaining one in yellow
-                        if min(last5) < 2 * self.sigma:
-                            self.colorYellow(last5, idx)
+        self.df = self.rule05_rule06(noPoints_window, noPointsOutOf_window, std_value)
 
         return self.df
 
 
-    def rule07(self):
+    def rule07(self, no_points=15, std_value=1):
         """
-        15 points in a row are within 1 std of the mean on either side of the mean.
+        Number of points in a row (e.g. 15) are within 1 std of the mean on either side of the mean.
+        :param no_points: int, minimum number of points to fulfill the condition
+        :param std_value: int, number of standard deviations, e.g. 2 for 2 std
         :return: dataframe containing outlier color coding.
         """
         for idx, elem in enumerate(self.data):
-            last15 = list(self.data.iloc[idx-14:idx+1])
-            if len(last15) == 15:
-                if min(last15) > (self.mean - self.sigma) and max(last15) < (self.mean + self.sigma):
-                    self.df["colorCode"].iloc[idx-14:idx+1] = "Red"
+            last_noPoints = list(self.data.iloc[idx - (no_points - 1):idx + 1])
+            if len(last_noPoints) == no_points:
+                if min(last_noPoints) > (self.mean - (std_value * self.sigma)) and \
+                        max(last_noPoints) < (self.mean + (std_value * self.sigma)):
+                    self.df["colorCode"].iloc[idx - (no_points - 1):idx + 1] = "Red"
 
         return self.df
 
 
-    def rule08(self):
+    def rule08(self, no_points=8, std_value=1):
         """
-        8 points in a row exist but none within 1 standard deviation of the mean,
+        Number of points in a row (e.g. 8) exist but none within x standard deviations of the mean,
         and the points are in both directions from the mean.
+        :param no_points: int, minimum number of points to fulfill the condition
+        :param std_value: int, number of standard deviations, e.g. 2 for 2 std
         :return: dataframe containing outlier color coding.
         """
         foundWithin = False
         for idx, elem in enumerate(self.data):
-            last8 = list(self.data.iloc[idx-7:idx+1])
-            if len(last8) == 8:
-                for el in last8:
-                    if el > (self.mean - self.sigma) and el < (self.mean + self.sigma):
+            last_noPoints = list(self.data.iloc[idx - (no_points - 1):idx + 1])
+            if len(last_noPoints) == no_points:
+                for el in last_noPoints:
+                    if (self.mean - (std_value * self.sigma)) < el < (self.mean + (std_value * self.sigma)):
                         foundWithin = True
                         break
                 if not foundWithin:
-                    self.df["colorCode"].iloc[idx-7:idx+1] = "Red"
+                    self.df["colorCode"].iloc[idx - (no_points - 1):idx + 1] = "Red"
             foundWithin = False
 
         return self.df
 
 
-    def getRule(self, ruleName):
+    def getRule(self, ruleName, no_points=None, noPoints_window=None, noPointsOutOf_window=None, std_value=None):
         """
         Apply trending rule of interest (chosen via drop down menu).
         :param ruleName: String, name of the chosen trending rule (drop down menu)
+        :param no_points: int, minimum number of points to fulfill the condition
+        :param noPoints_window: int, number of points for window of interest, e.g. 3 in "2 out of 3 points in a row"
+        :param noPointsOutOf_window: int, number of points within this window that fulfill a certain condition,
+                e.g. 2 in "2 out of 3 points in a row"
+        :param std_value: int, number of standard deviations, e.g. 2 for 2 std
         :return: dataframe containing the outlier color code of interest
         """
         if ruleName == "Rule 01":
-            df = self.rule01()
+            if std_value is None:
+                std_value = 3
+            df = self.rule01(std_value)
+
         elif ruleName == "Rule 02":
-            df = self.rule02()
+            if no_points is None:
+                no_points = 9
+            df = self.rule02(no_points)
+
         elif ruleName == "Rule 03":
-            df = self.rule03()
+            if no_points is None:
+                no_points = 6
+            df = self.rule03(no_points)
+
         elif ruleName == "Rule 04":
             df = self.rule04()
+
         elif ruleName == "Rule 05":
-            df = self.rule05()
+            if noPoints_window is None:
+                noPoints_window = 3
+            if noPointsOutOf_window is None:
+                noPointsOutOf_window = 2
+            if std_value is None:
+                std_value = 2
+            df = self.rule05(noPoints_window, noPointsOutOf_window, std_value)
+
         elif ruleName == "Rule 06":
-            df = self.rule06()
+            if noPoints_window is None:
+                noPoints_window = 5
+            if noPointsOutOf_window is None:
+                noPointsOutOf_window = 4
+            if std_value is None:
+                std_value = 1
+            df = self.rule06(noPoints_window, noPointsOutOf_window, std_value)
+
         elif ruleName == "Rule 07":
-            df = self.rule07()
+            if no_points is None:
+                no_points = 15
+            if std_value is None:
+                std_value = 1
+            df = self.rule07(no_points, std_value)
+
         elif ruleName == "Rule 08":
-            df = self.rule08()
+            if no_points is None:
+                no_points = 8
+            if std_value is None:
+                std_value = 1
+            df = self.rule08(no_points, std_value)
+
         else:
-            df = self.rule01()
+            df = self.rule01(std_value=3)
 
         return df
 
